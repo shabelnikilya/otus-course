142b79d7f0d2928ea2aba86ecdf55a0c34fdbb38
ishabelnik <ishabelnik@unislabs.com>
2022-09-11 11:29:51 +0200

    init hw16-io

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -2,5 +2,7 @@
  * Interface with dependency versions for Gradle.
  */
 public interface Versions {
-    String GUAVA = "31.1-jre";
+    String guava = "31.1-jre";
+
+    String glassfishJson = "2.0.1";
 }
