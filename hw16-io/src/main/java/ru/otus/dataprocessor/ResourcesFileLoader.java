a4df2b4b9e03b3b564d27ce45d6bf625b44cab66
ishabelnik <ishabelnik@unislabs.com>
2022-09-11 15:24:09 +0200

    Решение задания.

--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,36 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import jakarta.json.*;
 import ru.otus.model.Measurement;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ResourcesFileLoader implements Loader {
+    private final String fileName;
+    private final Gson gson = new GsonBuilder().create();
 
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+        JsonArray jsonValues = getJsonArray();
+        return jsonValues.stream()
+                .map(json -> gson.fromJson(json.toString(), Measurement.class))
+                .collect(Collectors.toList());
+    }
+
+    private JsonArray getJsonArray() {
+        JsonArray jsonValues;
+        try (JsonReader jsonReader = Json.createReader(
+                ResourcesFileLoader.class.getClassLoader().getResourceAsStream(fileName))
+        ) {
+            jsonValues = jsonReader.readArray();
+        }
+        return jsonValues;
     }
 }
